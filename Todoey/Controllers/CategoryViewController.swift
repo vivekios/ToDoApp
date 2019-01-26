e6685061dd0ba176c3b9a5250a9346c0dc888875
vivek singh <vivek.sin@hcl.com>
2019-01-26 23:45:13 +0530

    Added new category class and created relationships between entities.

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,41 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by vivek singh on 26/01/19.
+//  Copyright © 2019 vivek singh. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    // MARK: - TableView Datasource methods
+    
+    
+    // MARK: - TableView manipulation methods
+    
+    
+    // MARK: - Add New Categories
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+    
+    // MARK: - TableView Delegate methods
+    
+    
+    
+    
+    
+    
+    
+    
+}
